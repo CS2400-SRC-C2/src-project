d9dbd2b4bdac27c6ca92e9cab451c41b456348a0
Thet Wai <thetwai123456789.gmail.com>
2024-03-28 20:53:00 -0700

    map implmented with API key hidden in proxy server

---
+++ src/main.js
@@ -0,0 +1,75 @@
+// //use API key
+// ((g) => {
+//   var h,
+//     a,
+//     k,
+//     p = "The Google Maps JavaScript API",
+//     c = "google",
+//     l = "importLibrary",
+//     q = "__ib__",
+//     m = document,
+//     b = window;
+//   b = b[c] || (b[c] = {});
+//   var d = b.maps || (b.maps = {}),
+//     r = new Set(),
+//     e = new URLSearchParams(),
+//     u = () =>
+//       h ||
+//       (h = new Promise(async (f, n) => {
+//         await (a = m.createElement("script"));
+//         e.set("libraries", [...r] + "");
+//         for (k in g)
+//           e.set(
+//             k.replace(/[A-Z]/g, (t) => "_" + t[0].toLowerCase()),
+//             g[k]
+//           );
+//         e.set("callback", c + ".maps." + q);
+//         a.src = `https://maps.${c}apis.com/maps/api/js?` + e;
+//         d[q] = f;
+//         a.onerror = () => (h = n(Error(p + " could not load.")));
+//         a.nonce = m.querySelector("script[nonce]")?.nonce || "";
+//         m.head.append(a);
+//       }));
+//   d[l]
+//     ? console.warn(p + " only loads once. Ignoring:", g)
+//     : (d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)));
+// })({ key: "API KEY HIDDEN", v: "weekly" });
+
+// fetch("http://localhost:3000/fetchGoogleAPI", { mode: "cors" })
+//   .then(function (response) {
+//     console.log(response);
+//     eval(response);
+//   })
+//   .catch(function (err) {
+//     // Error :(
+//   });
+
+// Initialize and add the map
+
+async function initMap() {
+  // The location of Uluru
+  const position = {
+    lat: 34.057919,
+    lng: -117.821342,
+  };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  const map = new Map(document.getElementById("map"), {
+    zoom: 16,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
+}
+
+initMap();
