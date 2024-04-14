89e9c01007b3256b117ad640203846ac19efd44a
Thet Wai <thetwai123456789.gmail.com>
2024-04-13 23:07:31 -0700

    Working code that sets every marker from the FarmInfo.js

--- src/cropsInfo.js
+++
@@ -1,8 +0,0 @@
-export const cropLocations = [
-  [
-    { lat: 34.05483036641861, lng: -117.76191973068477 }, // Top-left corner
-    { lat: 34.05483036641861, lng: -117.76091973068477 }, // Top-right corner
-    { lat: 34.05492036641861, lng: -117.76091973068477 }, // Bottom-right corner
-    { lat: 34.05492036641861, lng: -117.76191973068477 }, // Bottom-left corner
-  ],
-];
